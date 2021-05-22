b94ca64fdd7b55687bdaf5da01e350239fe1f477
Anurag Rawat <anurag.rawat0201@gmail.com>
2021-05-22 23:30:09 +0100

    ♨️ add java-solution (formatting)

---
+++ Java/Java Output Formatting/solution.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("================================");
+
+        for (int i = 0; i < 3; i++) {
+            String s = scan.next();
+            int x = scan.nextInt();
+
+            System.out.printf("%-15s%03d%n", s, x);
+
+        }
+        System.out.println("================================");
+        scan.close();
+    }
+}
