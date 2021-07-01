52adf4466218e328f0d15c50ed1cec59da8e8f08
Anurag Rawat <anurag.rawat0201@gmail.com>
2021-07-01 21:24:15 +0100

    ✅ add new stage

---
+++ 30 Days of Code/Day 0: Hello, World./solution.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        // read input
+        String inputString = scan.nextLine();
+        
+        // Print Hello World
+        System.out.println("Hello, World.");
+        
+        // Print inputString
+        System.out.println(inputString);
+    }
+}
---
+++ 30 Days of Code/Day 1: Data Types/solution.py
@@ -0,0 +1,17 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Declare second integer, double, and String variables.
+second = int(input())
+double = float(input())
+string = str(input())
+
+# Print the sum of both integer variables on a new line.
+print(i+second)
+
+# Print the sum of the double variables on a new line.
+print(d+double)
+
+# Concatenate and print the String variables on a new line
+print(s+string)
