9c954ef6318883bc70121b6b67e312c63d746d0d
Anurag Rawat <anurag.rawat0201@gmail.com>
2022-01-25 23:07:27 +0000

    + divisibleSumPairs | O(n^2) ✓

---
+++ Algorithms/Implementation/Divisible Sum Pairs/Solution.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static int divisibleSumPairs(int n, int k, List<Integer> ar) {
+        int pairs = 0;
+        for (int i = 0; i < ar.size(); i++) {
+            for (int j = i+1; j < ar.size(); j++) {
+                if ((ar.get(i) + ar.get(j)) % k == 0) {
+                    pairs++;
+                }
+            }
+        }
+        return pairs;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.divisibleSumPairs(n, k, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+O
